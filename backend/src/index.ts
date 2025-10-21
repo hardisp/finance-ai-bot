e97fb73aa4d91afde6fc7c0b250d681bccbbcb26
Hardi <jiksdi12@gmail.com>
2025-10-21 23:13:11 +0800

    fix: update cors for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:3000" }));
 
 app.use("/api/google/oauth", googleOauthRouter);
 app.use("/api/hubspot/oauth", hubspotOauthRouter);
